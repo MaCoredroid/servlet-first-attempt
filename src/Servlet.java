3017517bfe96838f6bbb92947e6257eb82a785dc
unknown <coredroid0401@gmail.com>
2019-04-06 16:09:33 +0800

    second commit

--- src/Servlet.java
+++ src/Servlet.java
@@ -4,14 +4,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.io.PrintWriter;
 @WebServlet(name = "Servlet")
 public class Servlet extends HttpServlet {
+
+    private String message;
+    @Override
+    public void init() throws ServletException {
+        message = "Hello world, this message is from servlet!";
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
 
     }
 }
