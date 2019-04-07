e5eb10565d4ad0c137cc46e287dd7b56610be1a3
unknown <coredroid0401@gmail.com>
2019-04-07 17:51:44 +0800

    I am done

--- src/Servlet.java
+++ src/Servlet.java
@@ -1,17 +1,23 @@
+
+
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-@WebServlet(name = "Servlet")
-public class Servlet extends HttpServlet {
+import java.util.ArrayList;
+import java.util.List;
 
-    private String message;
+
+@WebServlet(name = "Servlet")
+
+public class Servlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
-        message = "Hello world, this message is from servlet!";
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -19,9 +25,15 @@ public class Servlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
+        List<String> list = new ArrayList<>();
+        list.add("item1");
+        list.add("item2");
+        list.add("item3");
+        String json = new Gson().toJson(list);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
 
     }
 }
