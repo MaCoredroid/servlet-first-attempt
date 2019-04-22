3677289ed4c3d4129078159ae2d76680d27b6eca
unknown <coredroid0401@gmail.com>
2019-04-22 19:25:15 +0800

    use servlet to put data in order and cart

---
+++ src/Cart.java
@@ -0,0 +1,49 @@
+import com.google.gson.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet(name = "Cart", urlPatterns = "/Cart")
+
+
+
+public class Cart extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Book> list1 = new ArrayList<>();
+
+
+        list1.add(new Book("King of the Ring", "John Ronald Reuel Tolkien",5000, "‎178-3-16-148410-0", 9, "./img/ring.jpg"));
+
+
+        String json1 = new Gson().toJson(list1);
+
+
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        PrintWriter out = response.getWriter();
+        out.print(json1);
+        out.flush();
+    }
+}
